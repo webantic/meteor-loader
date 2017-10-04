652d0724ba0c0e99d709a1c9cba4ede14771206b
Carl Evans <carl@tigatech.co.uk>
2017-10-04 16:39:32 +0100

    V1.0.1

--- loader.js
+++ loader.js
@@ -1,66 +1,96 @@
 /* globals Npm Plugin buildInjectorJs buildPackageJs */
 'use strict'
 
-if (process.env.INJECTOR_DEBUG) {
-  console.log('++++ WEBANTIC:LOADER ++++')
-}
-
-const path = Plugin.path
-const fs = Plugin.fs
-const mkdirp = Npm.require('mkdirp')
-const mapper = Npm.require('@webantic/dependency-mapper').default
-
-let npmDependencies = mapper(process.cwd())
-const thisModuleName = Object.keys(npmDependencies)[0]
-npmDependencies = npmDependencies[thisModuleName]
-
-// If there are any Meteor deps
-if (Object.keys(npmDependencies.allMeteorDependencies).length) {
-  // Ensure the webantic:node-injector package is included
-  let currentPackagesManifest = String(fs.readFileSync('.meteor/packages'))
-  if (currentPackagesManifest.indexOf('webantic:node-injector') === -1) {
-    currentPackagesManifest += '\nwebantic:node-injector\n'
-    fs.writeFileSync('.meteor/packages', currentPackagesManifest)
+class Loader {
+  constructor () {
+    this.path = Plugin.path
+    this.fs = Plugin.fs
+    this.mkdirp = Npm.require('mkdirp')
   }
 
-  // If the mapper failed, thisModuleName will be missing or === "default"
-  if (thisModuleName && thisModuleName !== 'default') {
-    const modulesWithMeteorDeps = []
-    const dependencies = npmDependencies.dependencies
-    for (const key in dependencies) {
-      if (Object.keys(dependencies[key].allMeteorDependencies).length) {
-        modulesWithMeteorDeps.push(key)
+  log (message) {
+    if (process.env.INJECTOR_DEBUG) {
+      console.log(message)
+    }
+  }
+  
+  error (message) {
+    if (process.env.INJECTOR_DEBUG) {
+      console.error(message)
+    }
+  }
+
+  ensureInjectorPresent () {
+    // Ensure the webantic:node-injector package is included
+    const packagesPath = '.meteor/packages'
+    let currentPackagesManifest = String(this.fs.readFileSync(packagesPath))
+    
+    const regExp = new RegExp(/^( *)webantic:node-injector/m)
+    if (!regExp.test(currentPackagesManifest)) {
+      currentPackagesManifest += '\nwebantic:node-injector\n'
+      this.fs.writeFileSync(packagesPath, currentPackagesManifest)
+    }
+  }
+
+  process () {
+    this.log('++++ WEBANTIC:LOADER ++++')
+
+    const mapper = Npm.require('@webantic/dependency-mapper').default
+    
+    let npmDeps = mapper(process.cwd())
+    const thisAppName = Object.keys(npmDeps)[0]
+    npmDeps = npmDeps[thisAppName]
+
+    const meteorDeps = npmDeps ? npmDeps.allMeteorDependencies : {}
+
+    // If there are any Meteor deps
+    if (Object.keys(meteorDeps).length) {
+      this.ensureInjectorPresent(process.cwd())
+
+      // If the mapper failed, thisAppName will be missing or === "default"
+      if (thisAppName && thisAppName !== 'default') {
+        const modulesWithMeteorDeps = []
+        const dependencies = npmDeps.dependencies
+
+        for (const key in dependencies) {
+          if (Object.keys(dependencies[key].allMeteorDependencies).length) {
+            modulesWithMeteorDeps.push(key)
+          }
+        }
+
+        const packageJsPath = 'packages/node-injector/package.js'
+        const injectorJsPath = 'packages/node-injector/injector.js'
+
+        let currentPackageJs, currentInjectorJs
+        try {
+          currentPackageJs = String(this.fs.readFileSync(packageJsPath))
+          currentInjectorJs = String(this.fs.readFileSync(injectorJsPath))
+        } catch (ex) {
+          this.error(ex.message)
+        }
+
+        const packageJs = buildPackageJs(meteorDeps)
+        const injectorJs = buildInjectorJs(meteorDeps, modulesWithMeteorDeps)
+    
+        if (currentPackageJs !== packageJs || currentInjectorJs !== injectorJs) {
+          console.log('\n*** Generating webantic:node-injector package ***')
+    
+          this.mkdirp.sync('packages/node-injector')
+    
+          if (currentPackageJs !== packageJs) {
+            this.fs.writeFileSync(packageJsPath, packageJs)
+          }
+    
+          if (currentInjectorJs !== injectorJs) {
+            this.fs.writeFileSync(injectorJsPath, injectorJs)
+          }
+        }
       }
     }
-
-    let currentPackageJs, currentInjectorJs
-    try {
-      currentPackageJs = String(fs.readFileSync('packages/node-injector/package.js'))
-      currentInjectorJs = String(fs.readFileSync('packages/node-injector/injector.js'))
-    } catch (ex) {
-      if (process.env.INJECTOR_DEBUG) {
-        console.error(ex)
-      }
-    }
-    const packageJs = buildPackageJs(npmDependencies.allMeteorDependencies)
-    const injectorJs = buildInjectorJs(npmDependencies.allMeteorDependencies, modulesWithMeteorDeps)
-
-    if (currentPackageJs !== packageJs || currentInjectorJs !== injectorJs) {
-      console.log('*** Generating webantic:node-injector package ***')
-
-      mkdirp.sync(path.resolve('packages/node-injector'))
-
-      if (currentPackageJs !== packageJs) {
-        fs.writeFileSync('packages/node-injector/package.js', packageJs)
-      }
-
-      if (currentInjectorJs !== injectorJs) {
-        fs.writeFileSync('packages/node-injector/injector.js', injectorJs)
-      }
-    }
+    
+    this.log('---- WEBANTIC:LOADER ----')
   }
 }
 
-if (process.env.INJECTOR_DEBUG) {
-  console.log('---- WEBANTIC:LOADER ----')
-}
+var loader = new Loader()
+loader.process()
--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 
 Package.describe({
   name: 'webantic:loader',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'A Meteor dependency loader',
   git: 'https://github.com/webantic/meteor-loader',
   documentation: 'README.md'
